f303f3603058e80e9639800bb4e1a60ead295632
melt-adzuki <rebanira02@gmail.com>
2022-08-07 21:46:58 +0900

    wip 4

--- src/lib.rs
+++ src/lib.rs
@@ -93,9 +93,9 @@ async fn auth(form: &FormData, ctx: &RouteContext<()>) -> Result<AuthState> {
             let kv_store = ctx.kv(KV_BINDING)?;
             let accounts: Accounts = kv_store.get("accounts").json().await?.ok_or("Couldn't fetch accounts")?;
 
-            match accounts.value.iter().find(|x| x.username == username) {
-                Some(account) => Ok(
-                    if account.password == password { AuthState::Ok }
+            match accounts.value.iter().find(|acct| acct.username == username) {
+                Some(acct) => Ok(
+                    if acct.password == password { AuthState::Ok }
                     else { AuthState::Err(Response::error("Invalid password", 401)) }
                 ),
                 None => Ok(AuthState::Err(Response::error("Invalid username", 401))),
